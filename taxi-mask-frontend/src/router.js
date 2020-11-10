501c8cc61c9d0309d2be5ab0acc38096933898e1
Baccouche Sami <sami.baccouche1998@gmail.com>
2020-11-10 15:36:10 +0100

    fixer un probleme

--- taxi-mask-frontend/src/router.js
+++ taxi-mask-frontend/src/router.js
@@ -1,13 +1,69 @@
-router.beforeEach((to, from, next) => {
-    const publicPages = ['/login', '/register', '/home'];
-    const authRequired = !publicPages.includes(to.path);
-    const loggedIn = localStorage.getItem('user');
-  
-    // trying to access a restricted page + not logged in
-    // redirect to login page
-    if (authRequired && !loggedIn) {
-      next('/login');
-    } else {
-      next();
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home' 
+import Order from '@/components/Order'
+import Cart from '@/components/Cart'
+import Layout from '@/components/Layout'
+import Profile from '@/views/Profile'
+import Login from '@/views/Login'
+import Register from '@/views/Register'
+import BoardAdmin from '@/views/BoardAdmin'
+import BoardUser from '@/views/BoardUser'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: Layout,
+      children:[
+        {
+          path:'/',
+          component:Home,
+          name:'Home'
+        },
+        {
+          path:'/order',
+          component:Order,
+          name:'Order'
+        },
+        {
+          path:'/cart',
+          component:Cart,
+          name:'Cart'
+        },
+        {
+          path: '/login',
+          component: Login
+        },
+        {
+          path: '/register',
+          component: Register
+        },
+        {
+          path: '/profile',
+          name: 'profile',
+          // lazy-loaded
+          component: Profile
+        },
+        {
+          path: '/admin',
+          name: 'admin',
+          // lazy-loaded
+          component: BoardAdmin
+        },
+        {
+          path: '/user',
+          name: 'user',
+          // lazy-loaded
+          component: BoardUser
+        }
+      ]
+
     }
-  });
+  ],
+    mode:'history'
+},
+
+  )
