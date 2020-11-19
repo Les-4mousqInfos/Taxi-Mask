a0013b2ee1c8f269866cbbba58a650bdf809db93
Benoît SCHULER <schuler@et.intechinfo.fr>
2020-11-19 10:49:09 +0100

    25 includes old files

--- taxi-mask-backend/src/test/java/fr/et/intechinfo/mousqinfos/taximask/AppTests.java
+++ taxi-mask-backend/src/test/java/fr/et/intechinfo/mousqinfos/taximask/AppTests.java
@@ -1,12 +1,7 @@
 package fr.et.intechinfo.mousqinfos.taximask;
 
-import fr.et.intechinfo.mousqinfos.taximask.controllers.ClientController;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
-import static org.assertj.core.api.Assertions.assertThat;
-
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
