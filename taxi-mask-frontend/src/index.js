cf1ebbabd11c3f1b9746b124d601f5566e8b3504
Benoît SCHULER <schuler@et.intechinfo.fr>
2020-11-23 14:54:20 +0100

    63 Try some changes

--- taxi-mask-frontend/src/index.js
+++ taxi-mask-frontend/src/index.js
@@ -1,9 +1,9 @@
 function component() {
     const element = document.createElement('div');
-    const config = require('./../build/webpack.base.conf.js');
-  
-    // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join('config/index.js');
+    //element.innerHTML = _.join('build/webpack.base.conf.js');
+    // Lodash, currently included via a script, is required for this line to work
+    
   
     return element;
   }
