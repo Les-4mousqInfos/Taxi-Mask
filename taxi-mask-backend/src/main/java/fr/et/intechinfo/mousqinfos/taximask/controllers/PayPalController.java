8adf7f87ee5558d8585737048b0ae8653bff7b39
Benoît SCHULER <schuler@et.intechinfo.fr>
2020-12-07 16:58:00 +0100

    57 Paypal backend infrastructure
    based on the expertise of Ihor Kosandiak

---
+++ taxi-mask-backend/src/main/java/fr/et/intechinfo/mousqinfos/taximask/controllers/PayPalController.java
@@ -0,0 +1,33 @@
+package fr.et.intechinfo.mousqinfos.taximask.controllers;
+
+import fr.et.intechinfo.mousqinfos.taximask.payload.PayPalClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/paypal")
+public class PayPalController {
+
+    private final PayPalClient payPalClient;
+    @Autowired
+    PayPalController(PayPalClient payPalClient){
+        this.payPalClient = payPalClient;
+    }
+
+    @PostMapping(value = "/make/payment")
+    public Map<String, Object> makePayment(@RequestParam("sum") String sum){
+        return payPalClient.createPayment(sum);
+    }
+
+    @PostMapping(value = "/complete/payment")
+    public Map<String, Object> completePayment(HttpServletRequest request){
+        return payPalClient.completePayment(request);
+    }
+
+}
